e181b67f09c86d4cbcf04ae53482bdbdb8c43911
Exequiel <alvarado.exequiel@gmail.com>
2023-06-18 20:10:58 -0400

    changes

--- zhangetsu/views.py
+++ zhangetsu/views.py
@@ -4,5 +4,12 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'index.html', {
         #context
-        
+        'message': 'Listado de productos',
+        'title': 'Productos',
+        'products':[
+            {'title':'Playera','price':5, 'stock': True},#productos
+            {'title':'Camisa','price':7, 'stock': True},
+            {'title':'Mochila','price':20, 'stock': False},
+        ]
+
     })
